45741b64e8788eb8a85fc74b03d8e126af86058e
Guest007 <puramv307@gmail.com>
2023-06-14 15:18:39 +0530

    added hotel entity

---
+++ userService/src/main/java/com/example/userService/model/Hotel.java
@@ -0,0 +1,16 @@
+package com.example.userService.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Hotel {
+    private String hid;
+    private String hname;
+    private String location;
+}
