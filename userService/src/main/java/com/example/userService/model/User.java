05c08b226f4fcd0efc3896b316f3f93a2e02ab72
Guest007 <puramv307@gmail.com>
2023-06-15 09:13:02 +0530

    refactored

--- userService/src/main/java/com/example/userService/model/User.java
+++ userService/src/main/java/com/example/userService/model/User.java
@@ -4,8 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.persistence.Transient;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,6 +13,9 @@ import java.util.List;
 @Table(name = "microUsers")
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class User {
     @Id
     private String uid;
