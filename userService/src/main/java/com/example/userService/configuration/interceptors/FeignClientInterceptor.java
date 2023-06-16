c67685dfe3fbae712d6c5befc3baedd1e5c44ca9
Guest007 <puramv307@gmail.com>
2023-06-16 11:27:37 +0530

    implemented feign and restTemplate interceptors

---
+++ userService/src/main/java/com/example/userService/configuration/interceptors/FeignClientInterceptor.java
@@ -0,0 +1,23 @@
+package com.example.userService.configuration.interceptors;
+
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.client.OAuth2AuthorizeRequest;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
+import org.springframework.stereotype.Component;
+
+@Configuration
+@Component
+public class FeignClientInterceptor implements RequestInterceptor {
+
+    @Autowired
+    OAuth2AuthorizedClientManager oAuth2AuthorizedClientManager;
+
+    @Override
+    public void apply(RequestTemplate requestTemplate) {
+       String token=oAuth2AuthorizedClientManager.authorize(OAuth2AuthorizeRequest.withClientRegistrationId("userServiceClient").principal("internal").build()).getAccessToken().getTokenValue();
+        requestTemplate.header("Authorization","Bearer"+token);
+    }
+}
